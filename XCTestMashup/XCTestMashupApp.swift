72d44e2c7fe38cd0747a98a3d4fb8f26f92bcfcf
Justin Lee <jhankilee@gmail.com>
2023-05-26 12:10:04 -0400

    Initial Commit

---
+++ XCTestMashup/XCTestMashupApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTestMashupApp.swift
+//  XCTestMashup
+//
+//  Created by Justin Lee on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct XCTestMashupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XCTestMashupTests/XCTestMashupTests.swift
@@ -0,0 +1,22 @@
+//
+//  XCTestMashupTests.swift
+//  XCTestMashupTests
+//
+//  Created by Justin Lee on 5/26/23.
+//
+
+import XCTest
+@testable import XCTestMashup
+
+final class XCTestMashupTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+
+}
